3aea6b6d923d6c8f9a3e4cabc9dd7e117e3d33c6
Jim Crossley <jim@crossleys.org>
2018-10-21 11:53:47 -0400

    Restructure things a bit
    Better matches the olm-catalog structure for the rh-operators
    
    Also changed find->ls to have more predictable ordering of the
    components of the script-generated CatalogSource ConfigMap.

--- etc/scripts/catalog.sh
+++ etc/scripts/catalog.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-DIR=$(cd $(dirname "$0")/.. && pwd)
+DIR=$(cd $(dirname "$0")/../../olm-catalog && pwd)
 INDENT="      "
 
 NAME="knative-operators"
 NAMESPACE="openshift-operator-lifecycle-manager"
 NAMEDISPLAY="Knative Operators"
 
-CRD=$(cat $(find $DIR -name '*crd.yaml') | grep -v -- "---" | sed "s/^/$INDENT/" | sed "s/ \( apiV\)/-\1/")
-CSV=$(cat $(find $DIR -name '*version.yaml') | sed "s/^/$INDENT/" | sed "s/ \( apiV\)/-\1/")
-PKG=$(cat $(find $DIR -name '*package.yaml') | sed "s/^/$INDENT/" | sed "s/ \( pack\)/-\1/")
+CRD=$(cat $(ls $DIR/*crd.yaml) | grep -v -- "---" | sed "s/^/$INDENT/" | sed "s/ \( apiV\)/-\1/")
+CSV=$(cat $(ls $DIR/*version.yaml) | sed "s/^/$INDENT/" | sed "s/ \( apiV\)/-\1/")
+PKG=$(cat $(ls $DIR/*package.yaml) | sed "s/^/$INDENT/" | sed "s/ \( pack\)/-\1/")
 
 cat <<EOF | sed 's/^  *$//'
 kind: ConfigMap
