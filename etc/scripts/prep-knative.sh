a6ec693403f282c45e63d5159c36f0c1be59499a
Jim Crossley <jim@crossleys.org>
2018-11-08 14:07:31 -0500

    Some of this we can get from addons now

--- etc/scripts/prep-knative.sh
+++ etc/scripts/prep-knative.sh
@@ -28,17 +28,6 @@ until oc login -u admin -p admin; do sleep 5; done;
 
 oc project myproject
 oc adm policy add-scc-to-user privileged -z default
-oc label namespace myproject istio-injection=enabled
-
-curl -s https://raw.githubusercontent.com/knative/docs/master/install/scripts/istio-openshift-policies.sh | bash
-
-oc apply -f https://storage.googleapis.com/knative-releases/serving/latest/istio.yaml
-
-oc get cm istio-sidecar-injector -n istio-system -oyaml | sed -e 's/securityContext:/securityContext:\\n      privileged: true/' | oc replace -f -
-
-while oc get pods -n istio-system | grep -v -E "(Running|Completed|STATUS)"; do sleep 5; done
-
-curl -s https://raw.githubusercontent.com/knative/docs/master/install/scripts/knative-openshift-policies.sh | bash
 
 # for the OLM console
 oc adm policy add-cluster-role-to-user cluster-admin system:serviceaccount:kube-system:default
